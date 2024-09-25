0784972df73bcf568adf8c169d104114d9fce935
DrBenSullivan <ben.sullivan1992@gmail.com>
2024-09-25 17:37:33 +0100

    Add Initial Database migration

--- OrdersAPI/Program.cs
+++ OrdersAPI/Program.cs
@@ -1,7 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using OrdersAPI.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+	options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
+
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
