68dff5cb0865fcf20995456cddd6afec4bc7e58d
DrBenSullivan <ben.sullivan1992@gmail.com>
2024-09-25 16:01:08 +0100

    Add Order Model

---
+++ OrdersAPI/Core/Models/Order.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace OrdersAPI.Core.Models
+{
+	public class Order
+	{
+		[Key]
+		public Guid? OrderId { get; set; }
+
+
+
+		[Required(ErrorMessage = "The Order Number must be provided")]
+		public string? OrderNumber { get; set; }
+
+
+
+		[Required(ErrorMessage = "The Customer Name must be provided.")]
+		[MaxLength(50, ErrorMessage = "The Customer Name must not exceed 50 characters.")]
+		public string? CustomerName { get; set; }
+
+
+		
+		[Required(ErrorMessage = "The Order Date must be provided.")] 
+		public DateTime? OrderDate { get; set; }
+
+
+
+		[Required(ErrorMessage = "The Total Cost of the order must be provided.")]
+		[Precision(18,2)]
+		[Range(0, Double.MaxValue, ErrorMessage = "The Total Cost of the order must not be negative.")]
+		public decimal? TotalCost { get; set; }
+	}
+}
